e62913c4d8ba06d127e98e0d13420d353266c00d
Sohel Ansar Shaikh <sohelshaikh24082000@gmail.com>
2022-09-08 22:59:13 +0530

    changing variables names in sum.java

--- sum.java
+++ sum.java
@@ -3,10 +3,10 @@ class sum {
 public static void main(String[] args){
 Scanner num=new Scanner(System.in);
 System.out.println("Enter first number: ");
-int a=num.nextInt();
+int x=num.nextInt();
 
 System.out.println("Enter second number: ");
-int b=num.nextInt();
-System.out.println("sum of two numbers is : "+(a+b));
+int y=num.nextInt();
+System.out.println("sum of two numbers is : "+(xx+y));
 }
 }
